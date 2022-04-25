1809045b219bdd2c4407bb2a72581e7f6da18b43
Geordan Sanchez <geordan.sanchez.contractor@bbva.com>
2022-04-24 23:33:37 -0500

    add Hero usecase and test

--- test/Hero.ts
+++ test/Hero.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat";
 
 describe("Hero", function() { 
     async function createHero() {
-        const Hero = await ethers.getContractFactory("Hero");
+        const Hero = await ethers.getContractFactory("TestHero");
         const hero = await Hero.deploy();
         await hero.deployed();
         
@@ -15,6 +15,10 @@ describe("Hero", function() {
 
     before(async function() {
         hero = await createHero()
+    });
+
+    it("should get zero hero array", async function() {
+        expect(await hero.getHeroes()).to.deep.equal([]);
     });
 
     it("should fail at creating hero", async function () {
@@ -30,4 +34,21 @@ describe("Hero", function() {
         
         expect(e.message.includes("Please send more money")).to.equal(true);
     });
- });
+
+    it("should fail at creating hero", async function () {
+        const hero = await createHero();
+
+        await hero.setRandom(69);
+        await hero.createHero(0, {
+            value: ethers.utils.parseEther("0.5")
+        });
+        const h = (await hero.getHeroes())[0];
+
+
+        // [ S , H , D, I, M]
+        // [ S, H , D , I]
+        // [ S, I , D]
+        expect(await hero.getMagic(h)).to.equal(16);
+        expect(await hero.getHealth(h)).to.equal(2);
+    });
+});
